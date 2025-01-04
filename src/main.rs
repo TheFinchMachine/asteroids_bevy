b5a756b70b4295f6131ca6e9a12e197c9856b986
Tyler Seacrest <tyler@seacrest.email>
2025-01-04 15:19:17 -0700

    hello world with greeting.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,28 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::new().add_systems(Update, hello_world).run();
+    App::new()
+    .add_systems(Startup, add_people)
+    .add_systems(Update, (hello_world, greet_people))
+    .run();
+}
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Elaina Proctor".to_string())));
+    commands.spawn((Person, Name("Resnzo Hume".to_string())));
+    commands.spawn((Person, Name("Zyna Nieves".to_string())));
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in &query {
+        println!("hello {}!", name.0);
+    }
 }
 
 fn hello_world() {
