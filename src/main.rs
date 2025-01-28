e70beebc9894b1b2c8d9459d65c3fd964f2c5839
Tyler Seacrest <tyler@seacrest.email>
2025-01-27 19:09:48 -0700

    remove manual ship creation

--- src/main.rs
+++ src/main.rs
@@ -41,7 +41,7 @@ fn spawn_ship(
     let mesh = asset_server.load(
         GltfAssetLabel::Primitive { mesh: 0, primitive: 0 }.from_asset("meshes/ship.gltf")
     );
-    
+
     let color = Color::srgb(0.8, 0.8, 1.0);
     let material = materials.add(color);
 
@@ -51,35 +51,6 @@ fn spawn_ship(
         MeshMaterial2d(material),
         Transform::from_scale(Vec3::new(50., 50., 50.)),
     ));
-}
-
-fn create_ship(scale: f32) -> Mesh {
-    // Create a new mesh using a triangle list topology, where each set of 3 vertices composes a triangle.
-    Mesh::new(PrimitiveTopology::TriangleList, RenderAssetUsages::default())
-        // Add 4 vertices, each with its own position attribute (coordinate in
-        // 3D space), for each of the corners of the parallelogram.
-        .with_inserted_attribute(
-            Mesh::ATTRIBUTE_POSITION,
-            vec![[0.0, 1.*scale, 0.0], [1.*scale, -1.5*scale, 0.0], [0.0, -1.*scale, 0.0], [-1.*scale, -1.5*scale, 0.0]]
-        )
-        // Assign a UV coordinate to each vertex.
-        .with_inserted_attribute(
-            Mesh::ATTRIBUTE_UV_0,
-            vec![[0.0, 1.0], [0.5, 0.0], [1.0, 0.0], [0.5, 1.0]]
-        )
-        // Assign normals (everything points outwards)
-        .with_inserted_attribute(
-            Mesh::ATTRIBUTE_NORMAL,
-            vec![[0.0, 0.0, 1.0], [0.0, 0.0, 1.0], [0.0, 0.0, 1.0], [0.0, 0.0, 1.0]]
-        )
-        // After defining all the vertices and their attributes, build each triangle using the
-        // indices of the vertices that make it up in a counter-clockwise order.
-        .with_inserted_indices(mesh::Indices::U32(vec![
-            // First triangle
-            0, 2, 1,
-            // Second triangle
-            3, 0, 2
-        ]))
 }
 
 fn spawn_camera(mut commands: Commands) {
