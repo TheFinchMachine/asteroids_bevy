1d80165daa89e829bb46a902960c28820eb47aaf
Tyler Seacrest <tyler@seacrest.email>
2025-02-01 22:46:38 -0700

    web deploy v0.1

---
+++ asteroids_bg.wasm.d.ts
@@ -0,0 +1,18 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const main: (a: number, b: number) => number;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_export_1: WebAssembly.Table;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_export_6: WebAssembly.Table;
+export const _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h1712694a8e9615f3: (a: number, b: number) => void;
+export const closure57412_externref_shim: (a: number, b: number, c: any) => void;
+export const closure58665_externref_shim: (a: number, b: number, c: any, d: any) => void;
+export const closure58669_externref_shim: (a: number, b: number, c: any) => void;
+export const _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__he5785d64516efe14: (a: number, b: number) => void;
+export const _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h3a5cb6e21b73198d: (a: number, b: number, c: number) => void;
+export const __wbindgen_start: () => void;
